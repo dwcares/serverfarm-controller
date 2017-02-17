72f2ebbf3454540196077a5561f66712e4154f1d
David Washington <david.washington@microsoft.com>
2017-02-17 10:34:51 -0600

    initial node server commit

---
+++ server/app.js
@@ -0,0 +1,53 @@
+var express = require('express');
+var app = express();
+app.use(express.static('public'));
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
+
+var serialport = require("serialport");	
+var SerialPort = serialport.SerialPort;
+
+SerialPort.list(function (err, ports) {
+  ports.forEach(function(port) {
+    console.log(port.comName);
+    console.log(port.pnpId);
+    console.log(port.manufacturer);
+  });
+});
+
+var portName = process.argv[2] ? process.argv[2] : 'com4';	
+
+// print out the port you're listening on:
+console.log("opening serial port: " + portName);	
+var myPort = new SerialPort(portName, { 
+	baudRate: 9600,
+
+	// look for return and newline at the end of each data packet:
+	parser: SerialPort.parsers.raw
+});
+
+io.on('connection', function(socket) {
+    console.log('user is connected')
+
+    socket.on('tx', function(tx) {
+        console.log('tx: '+ tx);
+        myPort.write(tx, function(err) {		
+          
+        }); 
+    });
+
+    myPort.on('data', function(rx) {
+            console.log('rx: '+ rx);
+            socket.emit('rx', "" + rx);
+    });    
+});
+
+app.get('/', function(req, res) {
+ res.sendFile(__dirname + '/public/index.html');
+});
+
+http.listen(port, function() {
+    console.log('listening on *: ' + port);   
+});
+
---
+++ server/public/protocol.js
@@ -0,0 +1,38 @@
+
+var protocol = {
+    execute: '!',
+    query: '?',
+    zone: {
+        familyroom: '1',
+        patio: '2',
+        mediaroom: '3',
+        kitchen: '4',
+        bathup: '5',
+        bathdown: '6',
+        dining: '7',
+        living: '8'
+    },
+    source: {
+        tv: 'MK0X3',
+        bluray: 'MK0X4',
+        sonos: 'MK0X5',
+        tuner1: 'MK0X7',
+        tuner2: 'MK0X8',
+        marantz: 'MK0X9',
+        xbox: 'MK0X10'
+    },
+    command: {
+        power: 'PT',
+        mute: 'MT',
+        volumeup: 'MK0X1',
+        volumedown: 'MK0X2', 
+        channelup: 'MK0X11',
+        channeldown: 'MK0X12'
+    },
+    macro: {
+        mvolumeup: '130MC',
+        mvolumedown: '131MC',
+        allzonesoff: 'AO'
+    },
+    commit: '+'
+}
